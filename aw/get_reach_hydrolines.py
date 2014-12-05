e9214bc0ae1cc58b7a6d64bdb7d5c5f295792915
knu2xs <knu2xs@gmail.com>
2014-12-04 21:56:53 -0800

    add open source licensing

--- aw/get_reach_hydrolines.py
+++ aw/get_reach_hydrolines.py
@@ -2,6 +2,20 @@
 author:     Joel McCune (joel.mccune+aw@gmail.com)
 dob:        04 Dec 2014
 purpose:    Provide a tool wrapper for extracting and saving reach hydrolines.
+
+    Copyright 2014 Joel McCune
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 # import modules
 import arcpy
