8a5f0feb8467ebfab8807f4b63dad1dd6ecbb82c
unknown <joel5174@OLY-MCCUNE7L.esri.com>
2015-08-26 17:09:54 -0700

    Update init

--- whitewater_analysis/utilities/__init__.py
+++ whitewater_analysis/utilities/__init__.py
@@ -1,14 +1,14 @@
 # expose functions at package scope
-from .reach_processing import (
+from reach_processing import (
     get_reach_line_fc,
     get_new_hydrolines,
     process_all_new_hydrolines,
     create_invalid_points_feature_class
 )
 
-from .publishing_tools import create_hydropoint_feature_class
+from publishing_tools import create_hydropoint_feature_class
 
-from .nhd_data import (
+from nhd_data import (
     get_nhd_subregion,
     build_subregion_directory
 )
